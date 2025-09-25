fabb812b9e30dbf20b4a387b8ff643867cb75f0e
Christopher Ren <christopher.x.ren@gmail.com>
2025-09-24 20:03:30 -0600

    Remove unused parse_env_flag helper

--- geovibes/tests/test_utils.py
+++ geovibes/tests/test_utils.py
@@ -1,19 +1,4 @@
-from geovibes.ui.utils import parse_env_flag, prepare_ids_for_query
-
-
-def test_parse_env_flag_truthy():
-    for value in ["1", "true", "YES", "On"]:
-        assert parse_env_flag(value) is True
-
-
-def test_parse_env_flag_falsey():
-    for value in ["0", "false", "NO", "off"]:
-        assert parse_env_flag(value) is False
-
-
-def test_parse_env_flag_unknown():
-    assert parse_env_flag("maybe") is None
-    assert parse_env_flag(None) is None
+from geovibes.ui.utils import prepare_ids_for_query
 
 
 def test_prepare_ids_for_query_casts_to_str():
